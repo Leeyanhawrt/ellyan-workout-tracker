b16bbaeb9e2165dc7e6741f7616242d93020badf
Leeyanhawrt <leeyan1108@gmail.com>
2023-10-24 17:18:56 -0400

    Init backend and development script

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,14 @@
-"use strict";
 // Express Setup
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
+
 const PORT = process.env.PORT || 8080;
+
 const app = express();
+
 // Middleware functions
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
