ced07f199871fe4e00218441ecd03d33dcdc20da
Leeyanhawrt <leeyan1108@gmail.com>
2023-10-25 17:28:40 -0400

    Add font family styles Amatic SC and Urbanist based off Ellissa's reccomendations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import "./styles.scss";
 
 function App() {
   return (
     <>
       <h1>Testing</h1>
+      <p>This is Ellissa and Leeyan's Workout Tracker</p>
     </>
   );
 }
