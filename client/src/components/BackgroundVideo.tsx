94f01a6b92d5c1d0b8af2ff94d68dacd73dd8387
Leeyanhawrt <leeyan1108@gmail.com>
2023-10-30 17:45:43 -0400

    Add BackgroundVideo component and css for headers

--- client/src/components/BackgroundVideo.tsx
+++ client/src/components/BackgroundVideo.tsx
@@ -4,16 +4,28 @@ interface BackgroundVideoProps {
   video: string;
   muted?: boolean;
   autoPlay?: boolean;
-  loop?: boolean;
   controls?: boolean;
+  loop?: boolean;
+  header?: string;
+  subheader?: string;
 }
 
 const BackgroundVideo: React.FC<BackgroundVideoProps> = ({
   video,
+  header,
+  subheader,
   ...configs
 }) => {
+  let backgroundText = (
+    <div className="header">
+      {header && <h1>{header}</h1>}
+      {subheader && <p>{subheader}</p>}
+    </div>
+  );
+
   return (
     <div id="video-container">
+      {backgroundText}
       <video {...configs} id="bg-video">
         <source src={video} type="video/mp4" />
       </video>
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,9 +2,19 @@ import BackgroundVideo from "../components/BackgroundVideo";
 import paddling_video from "../assets/videos/ndrc-paddling.mp4";
 
 const HomePage = () => {
+  let headerText = "Ellyan Workout Tracker";
+  let subheaderText = "Tracking to the Next Level";
+
   return (
     <section>
-      <BackgroundVideo video={paddling_video} muted autoPlay loop />
+      <BackgroundVideo
+        video={paddling_video}
+        header={headerText}
+        subheader={subheaderText}
+        muted
+        autoPlay
+        loop
+      />
     </section>
   );
 };
