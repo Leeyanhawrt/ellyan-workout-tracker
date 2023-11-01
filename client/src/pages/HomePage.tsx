d4b5ab423632b49a388f6c3de2657bffa17197c6
Leeyanhawrt <leeyan1108@gmail.com>
2023-11-01 17:45:06 -0400

    Add locale file and reading from it

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,17 +2,17 @@ import BackgroundVideo from "../components/BackgroundVideo";
 import FeaturesList from "../components/FeatureList";
 import paddling_video from "../assets/videos/ndrc-paddling.mp4";
 import "../assets/stylesheets/pages/_p_home.scss";
+import { useTranslation } from "react-i18next";
 
 const HomePage = () => {
-  let headerText = "Ellyan Workout Tracker";
-  let subheaderText = "Take Workout Tracking to the Next Level";
+  const { t } = useTranslation("", { keyPrefix: "homePage" });
 
   return (
     <>
       <BackgroundVideo
         video={paddling_video}
-        header={headerText}
-        subheader={subheaderText}
+        header={t("headerText")}
+        subheader={t("subheaderText")}
         muted
         autoPlay
         loop
