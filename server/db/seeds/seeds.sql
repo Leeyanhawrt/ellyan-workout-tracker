fe99a4a6efcaddad59c38b9ef0242ee22250b0d0
Leeyanhawrt <leeyan1108@gmail.com>
2023-11-02 19:11:06 -0400

    Use single quote in seed instead of double

--- server/db/seeds/seeds.sql
+++ server/db/seeds/seeds.sql
@@ -1,3 +1,3 @@
 -- TESTIMONIALS --
 INSERT INTO testimonials (first_name, last_name, title, review_description, profile_image_path)
-VALUES ('Leeyan', 'Haw', 'Tracking My Workouts Accelerated My Progress', "I was able to make gains super fast!", '~/src/assets/photos/testimonials/leeyan-haw.jpeg')
+VALUES ('Leeyan', 'Haw', 'Tracking My Workouts Accelerated My Progress', 'I was able to make gains super fast!', '~/src/assets/photos/testimonials/leeyan-haw.jpeg')
