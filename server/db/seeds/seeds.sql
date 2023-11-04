4bb42c4b408238300533336e77d72e23d0f8dde6
Leeyanhawrt <leeyan1108@gmail.com>
2023-11-04 18:47:07 -0400

    Edit 2nd testimonial title

--- server/db/seeds/seeds.sql
+++ server/db/seeds/seeds.sql
@@ -2,5 +2,5 @@
 INSERT INTO testimonials (first_name, last_name, title, review_description, profile_image_path)
 VALUES
 ('Leeyan', 'Haw', 'Tracking My Workouts Accelerated My Progress', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris commodo quis imperdiet massa tincidunt. Augue mauris augue neque gravida in. Netus et malesuada fames ac turpis egestas.', '../assets/photos/testimonials/leeyan-haw.jpeg'),
-('Wenjin', 'Haw', 'I Haven’t Gotten Injured Since Starting', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis id consectetur purus ut faucibus. At lectus urna duis convallis convallis tellus. Velit aliquet sagittis id consectetur.', '../assets/photos/testimonials/wenjin-haw.jpeg')
+('Wenjin', 'Haw', 'I Used to Get Injured All the Time', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis id consectetur purus ut faucibus. At lectus urna duis convallis convallis tellus. Velit aliquet sagittis id consectetur.', '../assets/photos/testimonials/wenjin-haw.jpeg')
 
